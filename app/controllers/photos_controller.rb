069ce5f931fa1a35fa561f137647ab5fc09ef5c4
RobynSpaulding <robynm27@gmail.com>
2022-10-29 16:05:24 -0700

    fixes photos create and update actions with dynamic user_id = current_user.id

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,33 +1,34 @@
 class PhotosController < ApplicationController
-  before_action :current_user
-  before_action :authenticate_user
+ 
   def index
     @photos = Photo.all
     render json: @photos.as_json
-  end
-
-  def create
-    @photo = Photo.new(
-      image_url: params[:image_url],
-      date: params[:date],
-      description: params[:description],
-      user_id: current_user.id
-    )
-    @photo.save
-    render json: @photo.as_json
   end
 
   def show
     @photo = Photo.find_by(id: params[:id])
     render json: @photo.as_json
   end
+   
+  def create
+    @photo = Photo.new(
+      image_url: params[:image_url],
+      date: params[:date],
+      description: params[:description],
+      user_id: current_user.id,
+    )
+    
+    @photo.save
+    render json: @photo.as_json
+  end
 
   def update
     @photo = Photo.find_by(id: params[:id])
+    
     @photo.image_url = params[:image_url] || @photo.image_url
     @photo.date = params[:date] || @photo.date
     @photo.description = params[:description] || @photo.description
-    @photo.user_id = params[:user_id] || @photo.user_id
+  
     @photo.save
     render json: @photo.as_json
   end
