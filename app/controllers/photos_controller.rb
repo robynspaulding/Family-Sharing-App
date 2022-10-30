176240b76ca43a0b4327a1a3138a2a28fc83a4e7
RobynSpaulding <robynm27@gmail.com>
2022-10-29 20:04:59 -0700

    fixes request.http errors by making user_id = current_user.id unlocks cors gem and Cors.rb file for React.js frontend access

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,36 +1,36 @@
 class PhotosController < ApplicationController
  
   def index
-    @photos = Photo.all
-    render json: @photos.as_json
+    photos = Photo.all
+    render json: photos
   end
 
   def show
-    @photo = Photo.find_by(id: params[:id])
-    render json: @photo.as_json
+    photo = Photo.find_by(id: params[:id])
+    render json: photo
   end
    
   def create
-    @photo = Photo.new(
+    photo = Photo.new(
       image_url: params[:image_url],
       date: params[:date],
       description: params[:description],
       user_id: current_user.id,
     )
     
-    @photo.save
-    render json: @photo.as_json
+    photo.save
+    render json: photo.as_json
   end
 
   def update
-    @photo = Photo.find_by(id: params[:id])
+    photo = Photo.find_by(id: params[:id])
     
-    @photo.image_url = params[:image_url] || @photo.image_url
-    @photo.date = params[:date] || @photo.date
-    @photo.description = params[:description] || @photo.description
+    photo.image_url = params[:image_url] || photo.image_url
+    photo.date = params[:date] || photo.date
+    photo.description = params[:description] || photo.description
   
-    @photo.save
-    render json: @photo.as_json
+    photo.save
+    render json: photo.as_json
   end
 
   def destroy
